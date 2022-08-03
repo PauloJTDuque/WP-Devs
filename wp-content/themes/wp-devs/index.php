d3c4e768ec7f1415f3d8b7882415e4fc1f964471
Paulo Duque <paulo.duque@terra.com.br>
2022-08-03 10:39:14 -0300

    WP Devs 0308

---
+++ wp-content/themes/wp-devs/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body <?php body_class();?>>
+    <h1>Esse é o meu Primeiro Tema</h1>
+    <div id="page" class="site"></div>
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+
+                </div>
+                <div class="searchbox">
+
+                </div>
+                
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+
+                </nav>
+            </section>
+        </header>
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                    <main id="main" class="site-main">
+                        <section class="hero">
+
+                        </section>
+                        <section class="services">
+
+                        </section>
+                        <section class="home-blog">
+
+                        </section>                            
+                    </main>
+            </div>        
+        </div>
+
+    </div>
+    <footer class="site-footer">
+
+    
+    </footer>
+</body>
+</html>
