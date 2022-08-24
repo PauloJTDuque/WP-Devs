a45fba4c3cfa1426c2787fe865c1e3a69ffa68ad
Paulo Duque <paulo.duque@terra.com.br>
2022-08-24 11:32:12 -0300

    WP Devs 2408

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -3,6 +3,7 @@
 function wpdevs_load_scripts(){
     wp_enqueue_style( 'wpdevs-style', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'), 'all' );
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap', array(), null);
+    wp_enqueue_script('dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', true );
 }
 // wpdevs-style = parâmetro  handle (identificador do arquivo na fila )
 // get_stylesheet_uri() = passa o caminho até o arquivo
@@ -10,3 +11,11 @@ function wpdevs_load_scripts(){
 //  '1.0'  - Versão da folha de estilo
 
 add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
+
+register_nav_menus(
+    array(
+        'wp_devs_main_menu' => 'Main Menu',
+        'wp_devs_footer_menu' => 'Footer Menu'
+    )
+);
+
