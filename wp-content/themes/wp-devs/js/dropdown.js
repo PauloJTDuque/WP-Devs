09cb522ca7564e4957f76957751c14cf172033f0
Paulo Duque <paulo.duque@terra.com.br>
2022-09-12 09:03:44 -0300

    WP Devs 1209A

--- wp-content/themes/wp-devs/js/dropdown.js
+++ wp-content/themes/wp-devs/js/dropdown.js
@@ -1,1 +1,12 @@
-console.log('Testing')
+let dropdown = document.querySelector('.menu'), // ul
+    submenu = document.querySelector('.sub-menu'), // ul li ul
+    buttonClick = document.querySelector('.check-button'), // button
+    hamburguer = document.querySelector('.menu-icon');
+
+buttonClick.addEventListener('click', () => {
+    dropdown.classList.toogle('show-dropdown');
+    if (submenu) {
+        submenu.classList.toogle('show-dropdown');
+    }
+    hamburguer.classList.toogle('animate-button');
+})
