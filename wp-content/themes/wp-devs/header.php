f80d5443c000effabf1066004a277db3185528c6
Paulo Duque <paulo.duque@terra.com.br>
2022-09-19 09:47:12 -0300

    WP Devs 1909A

--- wp-content/themes/wp-devs/header.php
+++ wp-content/themes/wp-devs/header.php
@@ -31,7 +31,6 @@
                                 <div class="bar2"></div>
                                 <div class="bar3"></div>
                             </div>
-
                         </button>
                         <?php wp_nav_menu( array( 'theme_location' => 'wp_devs_main_menu' , 'depth' => 2)); ?>
                     </nav>
--- wp-content/themes/wp-devs/js/dropdown.js
+++ wp-content/themes/wp-devs/js/dropdown.js
@@ -1,12 +1,13 @@
 let dropdown = document.querySelector('.menu'), // ul
-    submenu = document.querySelector('.sub-menu'), // ul li ul
-    buttonClick = document.querySelector('.check-button'), // button
-    hamburguer = document.querySelector('.menu-icon');
+submenu = document.querySelector('.sub-menu'), // ul li ul
+buttonClick = document.querySelector('.check-button'), // button
+hamburguer = document.querySelector('.menu-icon');
 
-buttonClick.addEventListener('click', () => {
+buttonClick.addEventListener( 'click', () => {
     dropdown.classList.toogle('show-dropdown');
-    if (submenu) {
-        submenu.classList.toogle('show-dropdown');
-    }
-    hamburguer.classList.toogle('animate-button');
+    submenu.classList.toogle('show-dropdown');
+    // if (submenu) {
+    //     submenu.classList.toogle('show-dropdown');
+    // }
+    //hamburguer.classList.toogle('animate-button');
 })
