c3f272bddca6e4e7df7e325bc1f27a6a21d89a29
Paulo Duque <paulo.duque@terra.com.br>
2022-10-06 09:58:34 -0300

    WP Devs 0610

--- wp-content/themes/wp-devs/index.php
+++ wp-content/themes/wp-devs/index.php
@@ -9,7 +9,30 @@
                         Services
                     </section>
                     <section class="home-blog">
-                        Blog
+                        <div class="container">
+                            <div class="blog-items">
+                                <?php
+                                    if( have_posts()):
+                                        while( have_posts()) : the_post();
+                                            //olhar template tags
+                                        ?>
+                                            <article>
+                                                <h2> <?php the_title(); ?> </h2>
+                                                <div class="meta-info">
+                                                    <p> Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                                    <p> Categories: <php the_category( ' ' ); ?></p>
+                                                    <p> Tags: <?php the_tags( ' ', ', '); ?></p>
+                                                </div>
+                                                <?php the_content(); ?>
+                                            </article>
+                                        <?php
+                                        endwhile;
+                                    else: ?>
+                                        <p>Nothing yet to be displayed!</p>
+                                <?php endif; ?>                                  
+                            </div>
+                        </div>
+         
                     </section>
                 </main>
             </div>
@@ -19,22 +42,3 @@
 
 
 
-<!-- <?php get_header(); ?>
-        
-        <div id="content" class="site-content">
-            <div id="primary" class="content-area">
-                    <main id="main" class="site-main">
-                        <section class="hero">
-                            Hero
-                        </section>
-                        <section class="services">
-                            Services
-                        </section>
-                        <section class="home-blog">
-                            Blog
-                        </section>                            
-                    </main>
-            </div>        
-        </div>
-
-<?php get_footer(); ?> -->
