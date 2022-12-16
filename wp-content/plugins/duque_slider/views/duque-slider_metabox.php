64e3f404a29cedab723272a4c26467f848e5b312
Paulo Duque <paulo.duque@terra.com.br>
2022-12-16 14:40:21 -0300

    WP Devs 1612A

--- wp-content/plugins/duque_slider/views/duque-slider_metabox.php
+++ wp-content/plugins/duque_slider/views/duque-slider_metabox.php
@@ -14,22 +14,22 @@
                 name="duque_slider_link_text" 
                 id="duque_slider_link_text" 
                 class="regular-text link-text"
-                value="<?php echo $meta[duque_slider_link_text] [0]; ?>"
+                value="<?php echo $meta['duque_slider_link_text'] [0]; ?>"
                 required
-            >
-        </td>
-    </tr>
-    <tr>
-        <th>
-            <label for="duque_slider_link_url">Link URL</label>
-        </th>
-        <td>
-            <input 
+                >
+            </td>
+        </tr>
+        <tr>
+            <th>
+                <label for="duque_slider_link_url">Link URL</label>
+            </th>
+            <td>
+                <input 
                 type="url" 
                 name="duque_slider_link_url" 
                 id="duque_slider_link_url" 
                 class="regular-text link-url"
-                value=""
+                value="<?php echo $meta['duque_slider_link_url'] [0]; ?>"
                 required
             >
         </td>
